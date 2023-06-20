58ea63cff14067ad14ddfff5b75a7a31803464cb
Andres Duque <andress1315@gmail.com>
2023-06-20 14:30:13 -0500

    feat: add validation in dashboard for token

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,21 @@
-
+import { useContext, useEffect } from 'react'
+import { UserContext } from '../context/user'
+import { useNavigate } from 'react-router-dom'
+import { useAxios } from '../hooks/useAxios'
 export const Dashboard: React.FC = () => {
+  const { token } = useContext(UserContext)
+  const { axiosRequest } = useAxios()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (token == null || token === undefined) {
+      navigate('/login')
+    }
+  }, [token])
+
+  axiosRequest({ method: 'get', endpoint: 'expenditures' }).then((response) => {
+    console.log(response)
+  })
   return (
     <h1>Dashboard</h1>
   )
