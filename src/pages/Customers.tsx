1ccd8b89fb9c93b34908c55de915fe50db124e90
Andres Duque <andress1315@gmail.com>
2023-06-30 09:09:58 -0500

    feat: add modal to show form edit

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -2,13 +2,23 @@ import { CreateCustomers } from '../components/CreateCustomers'
 import { useCustomers } from '../hooks/useCustomers'
 import { useEffect, useState } from 'react'
 import { type CustomerList } from '../types'
+import Swal from 'sweetalert2'
+import { Modal } from '../components/Modal'
+import { FormUpdateCustomer } from '../components/FormUpdateCustomer'
+
+interface ModalContent {
+  title: string
+  content: JSX.Element
+}
 
 export const Customers: React.FC = () => {
   const { getCustomers } = useCustomers()
   const [customer, setCustomer] = useState<any>([])
   const [customerSelected, setCustomerSelected] = useState<CustomerList>({})
-
+  const [open, setOpen] = useState(false)
+  const [contentModal, setContentModal] = useState<ModalContent>({})
   useEffect(() => {
+    setOpen(false)
     if (customer.length === 0) {
       getCustomers().then((response: { data: any }) => {
         const { data } = response.data
@@ -20,10 +30,32 @@ export const Customers: React.FC = () => {
   const handleSelectCustomer = (customer: CustomerList): void => {
     setCustomerSelected(customer)
   }
+
+  const handleUpdateCustomer = (): void => {
+    if (customerSelected.id != null) {
+      setContentModal({
+        title: 'Modificar Cliente',
+        content: <FormUpdateCustomer customer={customerSelected} refreshCustomer={setCustomer}/>
+      })
+      setOpen(true)
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        text: 'Seleccione un cliente',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+  }
   return (
     <div className="flex pt-2">
-      <div className="mx-4 pr-4  border-rose-300/30  border-r ">
+      <div className="mx-4 pr-4  border-rose-300/30  border-r flex flex-col">
         <CreateCustomers refreshCustomer={setCustomer}/>
+        <button
+          type="button"
+          onClick={handleUpdateCustomer}
+          className='bg-rose-300/70 text-white mt-2 rounded-lg py-1 hover:bg-rose-500/70'
+        >Modificar</button>
       </div>
       <div className="w-full overflow-auto h-[calc(100vh-5rem)] ">
         <table className="min-w-full divide-y mb-10">
@@ -52,6 +84,9 @@ export const Customers: React.FC = () => {
           </tbody>
         </table>
       </div>
+      <Modal open={open} setOpen={setOpen} title={contentModal.title}>
+        {contentModal.content }
+      </Modal>
     </div>
   )
 }
