e73b9e48de6e8ed6ff0a2066b228567ca165bd03
Andres Duque <andress1315@gmail.com>
2023-07-08 14:02:39 -0500

    feat: create reducer for users

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -1,15 +1,38 @@
-/* export const userInitialState = []
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
 
-export const reducer = (state, action) => {
-  const { type, payload } = action
-  switch (type) {
-    case 'SET_USER': {
-      const { user } = payload
-      const userSaveLocalStorage = JSON.stringify(user)
-      localStorage.setItem('user', userSaveLocalStorage)
+export interface UserState {
+  user: any
+  token: string | null
+  isLoged: boolean
+}
 
+export const initialState: UserState = {
+  user: null,
+  token: null,
+  isLoged: false
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<UserState>) => {
+      state.isLoged = true
+      state.user = action.payload.user
+      state.token = action.payload.token
+    },
+    logout: (state) => {
+      state.isLoged = false
+      state.user = null
+      state.token = null
+    },
+    setToken: (state, action: PayloadAction<UserState>) => {
+      state.token = action.payload.token
     }
   }
+})
 
-  return state
-} */
+export const userReducer = userSlice.reducer
+
+export const { login, logout, setToken } = userSlice.actions
