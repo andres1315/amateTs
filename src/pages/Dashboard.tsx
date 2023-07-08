015d70b958bd49182f7f3c7e7e6732b8c2b7f224
Andres Duque <andress1315@gmail.com>
2023-07-08 14:13:05 -0500

    feat: add redux to dashboiard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,30 +2,33 @@ import { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../context/user'
 import { useNavigate } from 'react-router-dom'
 import { HeaderDashboard } from '../components/NavbarDashboard'
-import { useExpenditures } from '../hooks/useExpenditures'
-import { useIncomes } from '../hooks/useIncomes'
+/* import { useExpenditures } from '../hooks/useExpenditures'
+import { useIncomes } from '../hooks/useIncomes' */
 import { HomeDashboard } from './HomeDashboard'
 import { Customers } from './Customers'
 import { Expenditures } from './Expenditures'
 import { Incomes } from './Incomes'
+import { useSelector } from 'react-redux'
+import { type RootState } from '../store'
 
 export const Dashboard: React.FC = () => {
   const [tap, setTap] = useState<string>('Home')
-
-  const { token } = useContext(UserContext)
+  const token = useSelector((state: RootState) => state.user.token)
+  /*   const { token } = useContext(UserContext) */
   const navigate = useNavigate()
-  const { getExpenditures } = useExpenditures()
-  const { getIncomes } = useIncomes()
+  /* const { getExpenditures } = useExpenditures()
+  const { getIncomes } = useIncomes() */
+
   useEffect(() => {
     if (token == null || token === undefined) {
       navigate('/login')
     }
-    getExpenditures().then((response) => {
+    /* getExpenditures().then((response) => {
       setData(response.data.data)
     })
     getIncomes().then((response) => {
       console.log(response.data.data)
-    })
+    }) */
   }, [token])
 
   const componentsTap = {
