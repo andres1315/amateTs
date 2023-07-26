e7623d553e869c44569672e8dfc79704c18a05d7
Andres Duque <andress1315@gmail.com>
2023-07-26 12:54:33 -0500

    feat: create middleware to save on localstorate

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { type Middleware, configureStore } from '@reduxjs/toolkit'
 import { userReducer } from './reducers/user'
 
+const persistanceLocalStorageMiddleware: Middleware = (store) => (next) => (action) => {
+  next(action)
+  const { type, payload } = action
+  if (type === 'user/login') {
+    localStorage.setItem('user', JSON.stringify(payload.user))
+    localStorage.setItem('token', payload.token)
+  }
+}
 export const store = configureStore({
   reducer: {
     user: userReducer
-  }
+  },
+  middleware: [persistanceLocalStorageMiddleware]
 })
 
 export type RootState = ReturnType<typeof store.getState>
